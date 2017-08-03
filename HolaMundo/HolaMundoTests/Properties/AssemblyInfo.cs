1a6416c5a9c3812d4a227a7256e253a345881aab
erick-monge <erick@firstfatory.com>
2017-08-03 08:58:02 -0600

    init operations

---
+++ HolaMundo/HolaMundoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HolaMundoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HolaMundoTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f927ae3-976f-4354-b92c-af26612e2f16")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HolaMundo/HolaMundoTests/SampleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HolaMundoTests
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void SimpleFirstTest()
+        {
+            Assert.AreEqual("1", "1");
+        }
+
+        [TestMethod]
+        public void SimpleSecondTest()
+        {
+            Assert.AreEqual("2", "2");
+        }
+    }
+}
